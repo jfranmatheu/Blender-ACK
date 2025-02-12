515ab9235dd45e533d1cc887b67a7786267d0806
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-02-12 01:11:49 +0100

    script to sync ackit folder (for addons using this template)

---
+++ sync-ackit.bat
@@ -0,0 +1,4 @@
+call git fetch upstream
+call git checkout main
+call git read-tree --prefix=project-name/ackit upstream/main:ackit_addon_template/ackit
+call git commit -m "Sync ackit folder from upstream"

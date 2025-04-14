d7a171b72ad6db345af362c55a1c4c85f7deb128
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2025-04-14 17:15:04 +0200

    added some icons (some needs attribution so the name of the platform is added in the filename and in the source code)

--- ackit_addon_template/icons.py
+++ ackit_addon_template/icons.py
@@ -10,4 +10,8 @@ class Icons(IconsViewer):
 
 	class MAIN(IconsEnum):
 		BLUE_BUTTON = icons_dirpath / "blue_button.png"
+		GALLERY_OUTLINE = icons_dirpath / "gallery_outline.png"
+		MUSIC_AUDIO_WAVES = icons_dirpath / "music_audio_waves.png"
+		ADD_TO_INBOX = icons_dirpath / "[icons8]add_to_inbox.png"
+		TEXTURE_SMALL = icons_dirpath / "[icons8]texture_small.png"
 
